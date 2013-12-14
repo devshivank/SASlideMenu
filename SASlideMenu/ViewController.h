3a477e19198b2738a69d1ed5ac013d844c3a7a02
shivank agarwal <shivank03mit@gmail.com>
2013-12-14 11:07:04 +0530

    Initial Commit

---
+++ SASlideMenu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SASlideMenu
+//
+//  Created by Anurag Gupta on 14/12/13.
+//  Copyright (c) 2013 Shivank Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SASlideMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SASlideMenu
+//
+//  Created by Anurag Gupta on 14/12/13.
+//  Copyright (c) 2013 Shivank Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
